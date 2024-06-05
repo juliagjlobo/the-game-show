c782e45a6e87cff8c7f0aa9ef6b12579d40884a0
Kimberli Depetris <kbi.dpts@gmail.com>
2024-06-05 19:52:15 -0300

    Musica
    Caminho paras musicas do jogo

---
+++ Assets/audiomanager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiomanager : MonoBehaviour
+{
+    [Header("--------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---------- Audio Clip -----------")]
+    public AudioClip background;
+    public AudioClip fase1;
+    public AudioClip fase2;
+    public AudioClip fase3;
+    public AudioClip fase4;
+    public AudioClip fase5;
+    public AudioClip car1;
+    public AudioClip car2;
+    public AudioClip car3;
+    public AudioClip car4;
+    public AudioClip crowd;
+    public AudioClip victory;
+
+}
